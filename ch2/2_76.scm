04062c151b226a259685c27f6de4db8dd6ea0554
Alex Clowes <alexclowes@gmail.com>
2020-09-16 20:45:22 +0100

    Exercises 2.75-2.76

---
+++ ch2/2_76.scm
@@ -0,0 +1,8 @@
+; Generic operations with explicit dispatch is simple to implement, but
+; difficult to extend.
+
+; Data-directed style makes it easy to add new types, but difficult to add new
+; operations.
+
+; Message-passing style makes it easy to add new operations, but difficult to
+; add new types.
