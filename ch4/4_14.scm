7b727cb4c57a4a2b278891c3c70fe59b3b029b5c
Alex Clowes <alexclowes@gmail.com>
2020-10-06 19:43:23 +0100

    Exercise 4.14

---
+++ ch4/4_14.scm
@@ -0,0 +1,3 @@
+; The map procedure of the underlying scheme does not expect to receive a
+; procedure object as defined in the metacircular evaluator instead of a
+; procedure from the underlying scheme.
